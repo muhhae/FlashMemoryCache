5e9fed41dee4a25dea097c7f08ec8b44b13dec3f
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-06-25 00:32:52 +0700

    Plot Updates

--- python/Tools/plotly_wrapper.py
+++ python/Tools/plotly_wrapper.py
@@ -12,17 +12,17 @@ templates.default = "plotly_dark"
 def Scatter(df: pd.DataFrame, **kwargs) -> go.Figure:
     fig = px.scatter(df, **kwargs)
     fig.update_layout(
-        font=dict(size=14),
+        font=dict(size=24),
         height=800,
         width=1000,
     )
     fig.update_traces(
-        marker_size=12,
-        marker_line=dict(width=2),
+        marker_size=24,
+        marker_line=dict(width=6),
         selector=dict(mode="markers"),
     )
-    fig.update_xaxes(showgrid=True)
-    fig.update_yaxes(showgrid=True)
+    fig.update_xaxes(showgrid=True, nticks=20)
+    fig.update_yaxes(showgrid=True, nticks=20)
     return fig
 
 
@@ -30,13 +30,13 @@ def Box(df: pd.DataFrame, **kwargs) -> go.Figure:
     fig = px.box(df, **kwargs)
     fig.update_xaxes(dtick=10)
     fig.update_layout(
-        font=dict(size=14),
+        font=dict(size=24),
         height=30 * len(df[kwargs.get("y")].unique()),
         width=1000,
         showlegend=False,
     )
-    fig.update_xaxes(showgrid=True)
-    fig.update_yaxes(showgrid=True)
+    fig.update_xaxes(showgrid=True, nticks=20)
+    fig.update_yaxes(showgrid=True, nticks=20)
     return fig
 
 
@@ -60,8 +60,11 @@ def VerticalCompositionBar(
         )
     fig.update_layout(
         barmode=mode,
+        font=dict(size=24),
         title_text=title,
         xaxis_title=xaxis_title,
         yaxis_title=yaxis_title,
     )
+    fig.update_xaxes(showgrid=True, nticks=10)
+    fig.update_yaxes(showgrid=True, nticks=10)
     return fig
