3393b308248ba9f20320747b02553fd00fe5bf91
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-06-27 06:28:03 +0700

    update download trace

--- scripts/download_trace.sh
+++ scripts/download_trace.sh
@@ -2,22 +2,28 @@
 
 input_file="$1"
 if [[ -z $input_file ]]; then
-  echo "[arg] input_file required"
-  exit 1
+    echo "[arg] input_file required"
+    exit 1
 fi
 
-mkdir -p /mnt/v0/traces
+output_dir="$2"
+if [[ -z $input_file ]]; then
+    echo "[arg] output_file required"
+    exit 1
+fi
+
+mkdir -p $output_dir
 
 while IFS= read -r link; do
     if [ -z "$link" ] || [[ "$link" == \#* ]]; then
         continue
     fi
     filename=$(basename $link)
-    if [ -s /mnt/v0/traces/$filename ]; then
+    if [ -s $output_dir/$filename ]; then
         echo "Skipping downloading trace, $filename exist"
         continue
     fi
-    wget -q -O /mnt/v0/traces/$filename $link
+    wget -q -O $output_dir/$filename $link
     echo "Finished downloading trace $filename"
 done < "$input_file"
 
