8ed4f7ba0fc1727347cfc6fba1ade00262fe6ec5
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-07-13 09:12:45 +0700

    offline_clock_v2 temp, not better than offline_clock currently.

--- src/cache/offline_clock_v2.cpp
+++ src/cache/offline_clock_v2.cpp
@@ -2,9 +2,12 @@
 
 #include <libCacheSim/cache.h>
 #include <libCacheSim/evictionAlgo.h>
+#include <sys/types.h>
 
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
+#include <print>
 
 #include "cache/additional_data.hpp"
 
@@ -17,40 +20,53 @@ void OfflineClockV2Evict(cache_t* cache, const request_t* req) {
     cache_obj_t* obj_to_evict = params->q_tail;
     while (obj_to_evict->clock.freq >= 1) {
         auto& data = additional_cache_data.objs_metadata[obj_to_evict->obj_id];
-        data.last_promotion = data.lifetime_freq;
+        if (data.current_access_after_promotion.contains(data.lifetime_freq) &&
+            data.current_access_after_promotion[data.lifetime_freq] >
+                data.final_access_after_promotion[data.lifetime_freq]) {
+            data.final_access_after_promotion[data.lifetime_freq] =
+                data.current_access_after_promotion[data.lifetime_freq];
+        }
 
         additional_cache_data.BeforeEvaluationTracking(obj_to_evict, req);
 
-        bool wasted = data.final_access_after_promotion.contains(data.last_promotion) &&
-                      data.final_access_after_promotion[data.last_promotion] == 0;
+        // if (data.final_access_after_promotion.contains(data.last_promotion)) {
+        //     std::println(
+        //         "access: {}", data.final_access_after_promotion[data.last_promotion]
+        //     );
+        // }
 
-        if (additional_cache_data.generate_datasets) {
-            auto features =
-                additional_cache_data.ObjectFeatures(data, cache, req, obj_to_evict);
-            features["wasted"] = wasted;
-            features["access_after_promotion"] =
-                data.final_access_after_promotion[data.last_promotion];
-            size_t counter = 0;
-            for (const auto& x : features) {
-                counter++;
-                additional_cache_data.datasets
-                    << x.first << (counter == features.size() - 1 ? "\n" : ",");
-            }
+        // bool wasted = data.final_access_after_promotion[data.lifetime_freq] < 1;
+        // bool wasted = data.final_access_after_promotion.contains(data.lifetime_freq) &&
+        //               data.final_access_after_promotion[data.lifetime_freq] < 1;
+        bool wasted = false;
+        if (data.final_access_after_promotion.contains(data.lifetime_freq)) {
+            uint64_t future_freq = data.final_access_after_promotion[data.lifetime_freq];
+            std::println("access: {}", future_freq);
+            bool wasted = future_freq < 1;
         }
+
+        // if (additional_cache_data.generate_datasets) {
+        //     auto features =
+        //         additional_cache_data.ObjectFeatures(data, cache, req, obj_to_evict);
+        //     features["wasted"] = wasted;
+        //     features["access_after_promotion"] =
+        //         data.final_access_after_promotion[data.last_promotion];
+        //     size_t counter = 0;
+        //     for (const auto& x : features) {
+        //         counter++;
+        //         additional_cache_data.datasets
+        //             << x.first << (counter == features.size() - 1 ? "\n" : ",");
+        //     }
+        // }
 
         additional_cache_data.BeforeEvictionTracking(obj_to_evict, req);
-
-        if (data.current_access_after_promotion[data.last_promotion] >
-            data.final_access_after_promotion[data.last_promotion]) {
-            data.final_access_after_promotion[data.last_promotion] =
-                data.current_access_after_promotion[data.last_promotion];
-        }
-        data.current_access_after_promotion.erase(data.last_promotion);
+        data.last_promotion = data.lifetime_freq;
 
         if (wasted) {
             break;
         }
 
+        data.current_access_after_promotion[data.lifetime_freq] = 0;
         additional_cache_data.OnPromotionTracking(obj_to_evict, req);
         obj_to_evict->clock.freq -= 1;
         params->n_obj_rewritten += 1;
