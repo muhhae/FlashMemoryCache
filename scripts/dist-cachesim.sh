03b31670ce7a23bf78629c24ba86e52b56f4a1b2
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-08-05 14:12:08 +0700

    make min_dram=1

--- scripts/dist-cachesim.sh
+++ scripts/dist-cachesim.sh
@@ -49,7 +49,8 @@ while IFS= read -r link; do
     size=$(stat --format="%s" "$file")
 
     gb=$(( (size + 1024*1024*1024 - 1) / (1024*1024*1024) ))
-    min_dram=$(( gb/3+1 ))
+    # min_dram=$(( gb/3+1 ))
+    min_dram=1
     priority=$(( 100/min_dram + 1 ))
 
     for cache_size in "${relative_cache_sizes[@]}"; do
