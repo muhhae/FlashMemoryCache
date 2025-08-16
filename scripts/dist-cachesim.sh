c366038dadb1dc5c72c07966ad35e19ce5c69ad9
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-08-16 19:32:17 +0700

    add cache_size into arg

--- scripts/dist-cachesim.sh
+++ scripts/dist-cachesim.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: bash $0 -r t[r]aces_txt -d traces_[d]ir -o [o]ut_dir -a [a]dd_desc -t [t]ask_out -g al[g]orithm -p add_[p]aram -i [i]ignore_obj_size -f [f]orce_replace"
+    echo "Usage: bash $0 -r t[r]aces_txt -d traces_[d]ir -o [o]ut_dir -a [a]dd_desc -t [t]ask_out -g al[g]orithm -p add_[p]aram -i [i]ignore_obj_size -f [f]orce_replace -s cache_[s]ize"
     exit 1
 }
 
 ignore_obj_size=false
 force_replace=false
 
-while getopts "fid:o:a:t:r:g:p:" opt; do
+while getopts "fid:o:a:t:r:g:p:s:" opt; do
     case $opt in
         d) traces_dir="$OPTARG" ;;
         o) out_dir="$OPTARG" ;;
@@ -17,6 +17,7 @@ while getopts "fid:o:a:t:r:g:p:" opt; do
         a) add_desc="$OPTARG" ;;
         g) algorithm="$OPTARG" ;;
         p) add_param="$OPTARG" ;;
+        s) cache_size="$OPTARG" ;;
         i) ignore_obj_size=true ;;
         f) force_replace=true ;;
         *) usage ;;
@@ -57,12 +58,12 @@ while IFS= read -r path; do
     if $ignore_obj_size; then
         output_path="$out_dir/log/$basename[$cache_size,ignore_obj_size,$algorithm$desc].json"
         if [ ! -s "$output_path" ] || $force_replace; then
-            echo "shell:$priority:$min_dram:1:~/FlashMemoryCache/build/cacheSimulator $file -a $algorithm $add_param -o $out_dir --ignore-obj-size -d ignore_obj_size,$algorithm$desc" >> $task_out
+            echo "shell:$priority:$min_dram:1:~/FlashMemoryCache/build/cacheSimulator $file -r $cache_size -a $algorithm $add_param -o $out_dir --ignore-obj-size -d ignore_obj_size,$algorithm$desc" >> $task_out
         fi
     else
         output_path="$out_dir/log/$basename[$cache_size,$algorithm$desc].json"
         if [ ! -s "$output_path" ] || $force_replace; then
-            echo "shell:$priority:$min_dram:1:~/FlashMemoryCache/build/cacheSimulator $file -a $algorithm $add_param -o $out_dir -d $algorithm$desc" >> $task_out
+            echo "shell:$priority:$min_dram:1:~/FlashMemoryCache/build/cacheSimulator $file -r $cache_size -a $algorithm $add_param -o $out_dir -d $algorithm$desc" >> $task_out
         fi
     fi
 done < "$traces_txt"
