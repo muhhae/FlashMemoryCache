c5f0aa6a53729b4beebfdc6fc611d84b01bf958b
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-09-29 02:04:27 +0700

    added dist-cachesim-2

--- scripts/dist-cachesim-2.sh
+++ scripts/dist-cachesim-2.sh
@@ -2,14 +2,9 @@
 
 ignore_obj_size=true
 traces_dir=/ltdata/data/oracleReuse
-out_dir=/mnt/nfs/lazy_promotions/simulation_results/hashed
 task_out=~/task
 
 > $task_out
-
-mkdir -p /mnt/nfs/results/prob
-mkdir -p /mnt/nfs/results/delay
-mkdir -p /mnt/nfs/results/batch
 
 while IFS= read -r path; do
     if [ -z "$path" ] || [[ "$path" == \#* ]]; then
@@ -27,9 +22,10 @@ while IFS= read -r path; do
     gb=$(( (size + 1024*1024*1024 - 1) / (1024*1024*1024) ))
     min_dram=$(( gb/4+1 ))
     priority=$(( 100/gb + 1 ))
-
-    echo "shell:$priority:$min_dram:1:cd /mnt/nfs/results/prob  && ~/bob-cachesim/build/bin/cachesim $file oracleGeneral prob -e prob=0.5 0.01 --ignore-obj-size 1" >> $task_out
-    echo "shell:$priority:$min_dram:1:cd /mnt/nfs/results/batch && ~/bob-cachesim/build/bin/cachesim $file oracleGeneral batch -e batch-size=0.5 0.01 --ignore-obj-size 1" >> $task_out
-    echo "shell:$priority:$min_dram:1:cd /mnt/nfs/results/delay && ~/bob-cachesim/build/bin/cachesim $file oracleGeneral delay -e delay-time=0.5 0.01 --ignore-obj-size 1" >> $task_out
+    for p in 0.1 0.2 0.3 0.4 0.5; do
+        echo "shell:$priority:$min_dram:1:cd /mnt/nfs/lazy_promotions/new_results/delay && ~/bob-cachesim/build/bin/cachesim $file oracleGeneral delay -e delay-time=$p 0.01 --ignore-obj-size 1" >> $task_out
+    done
+    # echo "shell:$priority:$min_dram:1:cd /mnt/nfs/lazy_promotions/new_results/prob && ~/bob-cachesim/build/bin/cachesim $file oracleGeneral prob -e prob=$p 0.01 --ignore-obj-size 1" >> $task_out
+    # echo "shell:$priority:$min_dram:1:cd /mnt/nfs/results/batch && ~/bob-cachesim/build/bin/cachesim $file oracleGeneral batch -e batch-size=0.5 0.01 --ignore-obj-size 1" >> $task_out
 
 done < ~/FlashMemoryCache/trace/all_datasets.txt
